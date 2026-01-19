8df2350ecab395ba22f667dfc5c67d6d8b2dd1c9
treejamie <me@treejamie.com>
2026-01-19 19:15:26 +0000

    first pass

--- lib/jamiec/content.ex
+++ lib/jamiec/content.ex
@@ -21,6 +21,7 @@ defmodule Jamiec.Content do
     Post
     |> order_by(desc: :inserted_at, desc: :id)
     |> Repo.all()
+    |> Repo.preload(:tags)
   end
 
   def list_posts(_scope) do
@@ -28,6 +29,7 @@ defmodule Jamiec.Content do
     |> where(status: :published)
     |> order_by(desc: :inserted_at, desc: :id)
     |> Repo.all()
+    |> Repo.preload(:tags)
   end
 
   @doc """
